7fdc4dac907e29aed9f99c0a047a4fe31fa6967e
527jbazerman <527Jbazerman@frhsd.com>
2026-02-20 10:35:34 -0500

    added op buttons

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+// what the user is currently typing ( as text)
+let typedNumberText = "";
+
+// the number we store for calculations
+let storedNumber = null;
+
+// the operator currently selected ( + - * /)
+let currentOperator = "";
+
+// used only for displaying the history
+let historyParts = [];
+
+//------------------
+// helper function
+//------------------
+
+function setStatus(message) {
+  document.getElementById("statusLine").textContent = message;
+}
+
+function showSymbol(operator) {
+  if (operator === "*") return "X";
+  if (operator === "/") return "&#x00F7";
+  if (operator === "-") return "&#x2212";
+  return operator;
+}
+
+function updateScreen() {
+  const display = document.getElementById("displayLine");
+  const history = document.getElementById("historyLine");
+  const status = document.getElementById("statusLine");
+
+  display.textContent = typedNumberText;
+}
+
 function pressNumber(digit) {
-    console.log(digit)
+  setStatus("");
+  if (typedNumberText === "0") {
+    typedNumberText = digit;
+  } else {
+    typedNumberText = typedNumberText + digit;
+  }
+
+  updateScreen();
 }
